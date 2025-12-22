fdb942ca3920a02b3f252521e0f79ab88f379360
htkien6706 <hoangkien06072006@gmail.com>
2025-12-22 17:29:39 +0700

    Finalize some details of update items before moving to nextjs

--- CRUD.js
+++ CRUD.js
@@ -3,8 +3,9 @@ import {renderHTML} from "./render-card-items.js";
 import { addProduct, deleteProduct, getProduct, KEY_LOCAL_STORAGE, initProducts } from "./products-services.js";
 
 let removed_index = null;
-let view_index = null;
+let current_index = null;
 
+// nếu mà muốn dùng products thì dùng cái này chung cho toàn bộ chương trình từ đầu đến cuối
 let products = initProducts(defaultProducts);
 renderHTML(products);
 console.log(products);
@@ -40,8 +41,30 @@ all_products.addEventListener('click', e=> {
 
     //have not yet handled the update button
     //handle it after accomplishing the add functionality
+
+    // completed !, move on to confirm update button and cancel update button
     else if(e.target.matches('.update-button')) {
         console.log("It's update button !");
+        e.stopPropagation();
+        const popup_overlay = document.querySelector('.popup-overlay');
+        popup_overlay.classList.add('active');
+        popup_overlay.querySelector('.update-items-overlay').classList.add('active');
+        popup_overlay.querySelector('.update-items-overlay').classList.add('showcase');
+
+        // first, find the index of the element we want to update
+        current_index = Array.from(document.querySelectorAll('.update-button')).indexOf(e.target);
+        console.log(current_index);
+
+        products = getProduct();
+
+        document.getElementById("product-name").value = products[current_index].product_description;
+        document.getElementById("preview-image").value = products[current_index].preview_image;
+        document.getElementById("original-price").value = products[current_index].original_price;
+        document.getElementsByTagName("discount-price").value = products[current_index].discount_price;
+        document.getElementById("detailed_description").value = products[current_index].detailed_description;
+
+
+        
     }
 
     // view actions when clicking in view button
@@ -49,9 +72,9 @@ all_products.addEventListener('click', e=> {
     else if(e.target.matches('.view-button')) {
         console.log("It's view button !");
         e.stopPropagation();
-        view_index = Array.from(document.querySelectorAll('.view-button')).indexOf(e.target);
-        console.log(view_index);
-        localStorage.setItem("view-index", "" + view_index);
+        current_index = Array.from(document.querySelectorAll('.view-button')).indexOf(e.target);
+        console.log(current_index);
+        localStorage.setItem("view-index", "" + current_index);
 
         window.location.href = "product-details.html";
     }
@@ -70,7 +93,7 @@ const popup_overlay = document.querySelector('.popup-overlay')
 popup_overlay.addEventListener('click', e=> {
     e.stopPropagation();
 
-    //cancel deleting action
+    //cancel deleting action (completed!)
     if(e.target.matches('.cancel-button')) {
         console.log("It's cancel button !");
         popup_overlay.classList.remove('active');
@@ -79,7 +102,7 @@ popup_overlay.addEventListener('click', e=> {
 
     }
 
-    // confirm action of deleting 
+    // confirm action of deleting (completed !)
     else if(e.target.matches('.confirm-delete-button')) {
         console.log("Confirm deleting items !");
         popup_overlay.classList.remove('active');
@@ -87,6 +110,10 @@ popup_overlay.addEventListener('click', e=> {
         popup_overlay.querySelector('.delete-items-overlay').classList.remove('showcase');
 
         deleteProduct(products, removed_index);
+
+        //reupdate the products after deleting items
+        // if no use localStorage, the data interexchanged between two files is just temporary and just vanish when refresh..
+        // we always want permanently store data, but now, no backend, just use localStorage
         products = getProduct();
         renderHTML(products);
     }
@@ -128,6 +155,7 @@ popup_overlay.addEventListener('click', e=> {
         }
 
         addProduct(products, added_product);
+        //reupdate the localStorage for permanent storage of items
         products = getProduct();
 
         renderHTML(products);
@@ -143,6 +171,57 @@ popup_overlay.addEventListener('click', e=> {
         document.querySelector('.add-items-overlay').classList.remove("active");
     }
 
+    //completed !
+    if(e.target.matches('.cancel-update-button')) {
+        console.log("cancel button of update functionality clicked!");
+        e.stopPropagation();
+        e.preventDefault();
+
+        document.querySelector('.popup-overlay').classList.remove('active');
+        document.querySelector('.update-items-overlay').classList.remove('showcase');
+        document.querySelector('.update-items-overlay').classList.remove('active');
+    }
+
+    if(e.target.matches('.confirm-update-button')) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        document.querySelector('.popup-overlay').classList.remove('active');
+        document.querySelector('.update-items-overlay').classList.remove('showcase');
+        document.querySelector('.update-items-overlay').classList.remove('active');
+
+
+        products = getProduct();
+
+        //Step 0: save all the modified dât to localStorage, and next step update it
+        products[current_index].product_description = document.getElementById("product-name").value;
+        products[current_index].preview_image = document.getElementById("preview-image").value;
+        products[current_index].original_price = document.getElementById("original-price").value;
+        products[current_index].discount_price = document.getElementById("discount-price").value;
+
+        // Step 1: update teh localStorage for future storage
+        localStorage.setItem(KEY_LOCAL_STORAGE, products);
+
+        products = getProduct();
+
+        //Step 2: using JS, fetch data for main screen, prepare for changing UI
+        // note that js changed cannot lead to DOM change, must render UI again
+        products[current_index].querySelector('.preview-image').src = products[current_index].preview_image;
+        
+        //children[0] synonymous with <p> product description
+        products[current_index].querySelector('.product-description').children[0].value = products[current_index].product_description;
+        products[current_index].getElementById("original-price").value = products[current_index].original_price;
+        products[current_index].getElementById("discount-price").value = products[current_index].discount_price;
+
+        //Step 3: render the UI of the webpage again to also change the DOM as wish
+        
+
+
+
+
+
+    }
+
 
 })
 
@@ -153,6 +232,10 @@ document.querySelector('.add-product-button').addEventListener('click', e=> {
     document.querySelector('.add-items-overlay').classList.add("showcase");
     document.querySelector('.add-items-overlay').classList.add("active")
 })
+
+console.log(products[products.length - 1]);
+
+
 
 
 
