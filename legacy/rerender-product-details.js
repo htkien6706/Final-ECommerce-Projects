f425a0e01be11de6f15c3116ec23c3b77f9694db
htkien6706 <hoangkien06072006@gmail.com>
2026-01-21 02:02:20 +0700

    configure succesfully, now moving to nestjs

--- legacy/rerender-product-details.js
+++
@@ -1,5 +0,0 @@
-import { KEY_LOCAL_STORAGE } from "./products-services.js";
-import { renderHTML_products } from "./render-product-details.js";
-const view_idx = Number(localStorage.getItem("view-index"));
-
-renderHTML_products(JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE)), view_idx);
--- legacy/test.js
+++
@@ -1,4 +0,0 @@
-const arr1 = [1,2,3,4,54];
-const arr2 = arr1.filter((num) => {num > 10});
-
-console.log(arr1 === arr2)
